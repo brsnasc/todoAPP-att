8660233131a1bd01b0e18c2a81907d9d08331acd
brsnasc <brsnasilvac@outlook.com>
2022-06-23 17:46:56 -0300

    First Commit

---
+++ app/src/main/java/com/generation/todoapplication/adapter/TaskClickListener.kt
@@ -0,0 +1,8 @@
+package com.generation.todoapplication.adapter
+
+import com.generation.todoapplication.model.Tarefa
+
+interface TaskClickListener {
+
+    fun onTaskClickListener(tarefa: Tarefa)
+}
